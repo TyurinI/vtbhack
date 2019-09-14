31263fceb0e6d07ba884fcc654b2aff781f2fb43
Ivan <iw.turin2012@yandex.ru>
2019-09-14 12:27:22 +0300

    initial commit

---
+++ src/main/kotlin/ru/vtb/vtbhack/VtbhackApplication.kt
@@ -0,0 +1,11 @@
+package ru.vtb.vtbhack
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VtbhackApplication
+
+fun main(args: Array<String>) {
+    runApplication<VtbhackApplication>(*args)
+}
